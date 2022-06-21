bce8ad0b9fa1aae5fadf91197e3012cd8dc62059
rudihartono7 <rudi.hartono@outlook.com>
2022-06-21 16:10:42 +0700

    add new api detail

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,4 +29,15 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+    [HttpGet(Name = "GetWeatherForecast-Detail")]
+    public IEnumerable<WeatherForecast> GetDetail(string id)
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
